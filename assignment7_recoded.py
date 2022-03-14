c9b35b96d6f781f7f10a79da98a56d9069831144
Karl Mark Funcion <karlmarkpfuncion@iskolarngbayan.pup.edu.ph>
2022-03-14 15:44:57 +0800

    assignment7 recoded, and assignment8 commit

---
+++ assignment7_recoded.py
@@ -0,0 +1,12 @@
+
+#list of grades.
+list_of_grades = [92, 90, 94, 95, 90]
+
+def get_average(list_of_grades):
+    #formula for getting the average in a list
+    average_formula = sum(list_of_grades)/ len(list_of_grades)
+
+    print(average_formula)
+    #return average_formula
+
+get_average(list_of_grades)
---
+++ assignment8.py
@@ -0,0 +1,10 @@
+
+variable_a = 12
+variable_b = 32
+
+
+def variable_swappper(var_a, var_b):
+    new_var_a = var_b
+    new_var_b = var_a
+
+    return (var_a, var_b)
