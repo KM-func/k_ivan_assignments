6444ae6f0c1136b1fb4394e24ee1cc5b19d1972e
Karl Mark Funcion <karlmarkpfuncion@iskolarngbayan.pup.edu.ph>
2022-03-15 14:47:03 +0800

    assignment9 completed

--- assignment9.py
+++ assignment9.py
@@ -1,7 +1,21 @@
 
+#variables for the functions
 min_range = 7
-max_range =62
+max_range = 62
 multiples_of = 7
 
 def multiple_finder(min, max, multiple):
-    pass
+    sum_of_multiples = 0 #empty function to contain the sum of multiples
+
+    for i in range(min, max+1): #don't forget to add a +1 to the max index
+
+        if (i % multiple) == 0: #divide formula to check if i is a multiple
+            print(i)
+            sum_of_multiples += i #adds this all up to print the sum of all multiples
+        else:
+            pass
+
+    #return sum_of_multiples #just in case 3 =3
+    print(f'Sum of all multiples is: {sum_of_multiples}')
+
+multiple_finder(min_range, max_range, multiples_of) #calling the function
